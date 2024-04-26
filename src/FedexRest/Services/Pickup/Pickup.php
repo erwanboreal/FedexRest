18656b4ab5fc802752fa4a48f6f6c420d62b2ad5
erwan <erwan@boreal-business.net>
2024-04-26 15:47:20 +0200

    Pickup service

---
+++ src/FedexRest/Services/Pickup/Pickup.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace FedexRest\Services\Pickup;
+
+use Exception;
+use FedexRest\Entity\Person;
+use FedexRest\Exceptions\MissingAccessTokenException;
+use FedexRest\Services\AbstractRequest;
+use GuzzleHttp\Exception\GuzzleException;
+
+class Pickup extends AbstractRequest {
+
+    protected Person $recipient;
+    protected string $readyDatestamp = '';
+    protected string $pickupType = '';
+    protected string $carrierCode = '';
+    protected string $customerCloseTime = '';
+    protected int $accountNumber;
+
+    /**
+     * @return string
+     */
+    public function setApiEndpoint(): string {
+        return '/pickup/v1/pickups';
+    }
+
+    /**
+     * @param string $pickupType
+     * @return $this
+     */
+    public function setPickupType(string $pickupType): Pickup {
+        $this->pickupType = $pickupType;
+        return $this;
+    }
+
+    /**
+     * @param int $accountNumber
+     * @return $this
+     */
+    public function setAccountNumber(int $accountNumber): Pickup {
+        $this->accountNumber = $accountNumber;
+        return $this;
+    }
+
+    /**
+     * @param string $carrierCode
+     * @return $this
+     */
+    public function setCarrierCode(string $carrierCode): Pickup {
+        $this->carrierCode = $carrierCode;
+        return $this;
+    }
+
+    /**
+     * @param string $readyDatestamp
+     * @return $this
+     */
+    public function setReadyDatestamp(string $readyDatestamp): Pickup {
+        $this->readyDatestamp = $readyDatestamp;
+        return $this;
+    }
+
+    /**
+     * @param string $customerCloseTime
+     * @return $this
+     */
+    public function setCustomerCloseTime(string $customerCloseTime): Pickup {
+        $this->customerCloseTime = $customerCloseTime;
+        return $this;
+    }
+
+    /**
+     * @param Person $recipient
+     * @return $this
+     */
+    public function setRecipient(Person $recipient): Pickup {
+        $this->recipient = $recipient;
+        return $this;
+    }
+
+    /**
+     * @throws MissingAccessTokenException
+     * @throws GuzzleException
+     */
+    public function request() {
+        parent::request();
+
+        try {
+            $query = $this->http_client->post($this->getApiUri($this->api_endpoint), [
+                'json' => [
+                    'associatedAccountNumber' => [
+                        "value" => $this->accountNumber
+                    ],
+                    'originDetail' => $this->prepare(),
+                    'carrierCode' => $this->carrierCode,
+                    'pickupType' => $this->pickupType
+                ]
+            ]);
+            return ($this->raw === true) ? $query : json_decode($query->getBody()->getContents());
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function prepare(): array {
+        return [
+            'pickupLocation' => $this->recipient->prepare(),
+            'readyDateTimestamp' => $this->readyDatestamp,
+            'customerCloseTime' => $this->customerCloseTime,
+        ];
+    }
+}
---
+++ src/FedexRest/Services/Pickup/PickupAvailability.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace FedexRest\Services\Pickup;
+
+use Exception;
+use FedexRest\Entity\Address;
+use FedexRest\Exceptions\MissingAccessTokenException;
+use FedexRest\Services\AbstractRequest;
+use GuzzleHttp\Exception\GuzzleException;
+
+class PickupAvailability extends AbstractRequest {
+
+    protected Address $pickupAddress;
+    protected array $pickupRequestType = [];
+    protected array $carriers = [];
+    protected string $countryRelationship = '';
+    protected string $dispatchDate = '';
+
+    /**
+     * @return string
+     */
+    public function setApiEndpoint(): string {
+        return '/pickup/v1/pickups';
+    }
+
+    /**
+     * @param string $countryRelationship
+     * @return $this
+     */
+    public function setCountryRelationship(string $countryRelationship): PickupAvailability {
+        $this->countryRelationship = $countryRelationship;
+        return $this;
+    }
+
+    /**
+     * @param array $carriers
+     * @return $this
+     */
+    public function setCarriers(array $carriers): PickupAvailability {
+        $this->carriers = $carriers;
+        return $this;
+    }
+
+    /**
+     * @param array $pickupRequestType
+     * @return $this
+     */
+    public function setPickupRequestType(array $pickupRequestType): PickupAvailability {
+        $this->pickupRequestType = $pickupRequestType;
+        return $this;
+    }
+
+    /**
+     * @param string $dispatchDate
+     * @return $this
+     */
+    public function setDispatchDatestamp(string $dispatchDate): PickupAvailability {
+        $this->dispatchDate = $dispatchDate;
+        return $this;
+    }
+
+    /**
+     * @param Address $pickupAddress
+     * @return $this
+     */
+    public function setPickupAddress(Address $pickupAddress): PickupAvailability {
+        $this->pickupAddress = $pickupAddress;
+        return $this;
+    }
+
+    /**
+     * @throws MissingAccessTokenException
+     * @throws GuzzleException
+     */
+    public function request() {
+        parent::request();
+
+        try {
+            $query = $this->http_client->post($this->getApiUri($this->api_endpoint), [
+                'json' => [
+                    'pickupAddress' => $this->pickupAddress->prepare(),
+                    'pickupRequestType' => $this->pickupRequestType,
+                    'carriers' => $this->carriers,
+                    'countryRelationship' => $this->countryRelationship,
+                    'dispatchDate' => $this->dispatchDate
+                ]
+            ]);
+            return ($this->raw === true) ? $query : json_decode($query->getBody()->getContents());
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+}
