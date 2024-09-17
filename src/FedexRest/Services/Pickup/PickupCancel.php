05497ebe56f57efb04130244628e999ada4c086f
erwan <erwan@boreal-business.net>
2024-09-17 15:52:34 +0200

    pickup cancel

---
+++ src/FedexRest/Services/Pickup/PickupCancel.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace FedexRest\Services\Pickup;
+
+use Exception;
+use FedexRest\Exceptions\MissingAccessTokenException;
+use FedexRest\Services\AbstractRequest;
+use GuzzleHttp\Exception\GuzzleException;
+
+class PickupCancel extends AbstractRequest {
+
+    protected int $accountNumber;
+    protected string $pickupConfirmationCode = '';
+    protected string $scheduledDate = '';
+
+    /**
+     * @return string
+     */
+    public function setApiEndpoint(): string {
+        return "/pickup/v1/pickups/cancel";
+    }
+
+    /**
+     * @param int $accountNumber
+     * @return $this
+     */
+    public function setAccountNumber(int $accountNumber): Pickup {
+        $this->accountNumber = $accountNumber;
+        return $this;
+    }
+
+    /**
+     * @param string $pickupConfirmationCode
+     * @return $this
+     */
+    public function setPickupConfirmationCode(string $pickupConfirmationCode): Pickup {
+        $this->pickupConfirmationCode = $pickupConfirmationCode;
+        return $this;
+    }
+
+    /**
+     * @param string $scheduledDate
+     * @return $this
+     */
+    public function setScheduledDate(string $scheduledDate): Pickup {
+        $this->scheduledDate = $scheduledDate;
+        return $this;
+    }
+
+    /**
+     * @throws MissingAccessTokenException
+     * @throws GuzzleException
+     */
+    public function request() {
+        parent::request();
+
+        try {
+            $query = $this->http_client->post($this->getApiUri($this->api_endpoint), [
+                'json' => $this->prepare(),
+                'http_errors' => FALSE,
+            ]);
+            return ($this->raw === true) ? $query : json_decode($query->getBody()->getContents());
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function prepare(): array {
+        return [
+            'associatedAccountNumber' => [
+                "value" => $this->accountNumber
+            ],
+            'pickupConfirmationCode' => $this->pickupConfirmationCode,
+            'scheduledDate' => $this->scheduledDate,
+        ];
+    }
+
+
+
+}
